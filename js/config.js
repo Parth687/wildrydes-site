b861740c97456abf9925adc9cdee4df6cd515a71
Parth687 <kakadiyaparth687@gmail.com>
2025-01-20 11:04:11 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-north-1_trN0VCHA0', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 's3rlqqurcn1ddiii7ovr50cfg', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'eu-north-1_MSOQU8QMj', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '15cj4t6vmetoku8dj9oude5sao', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-north-1' // e.g. us-east-2
     },
     api: {
